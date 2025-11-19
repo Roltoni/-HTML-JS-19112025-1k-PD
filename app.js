a6bd8d795ee325fc71145b658c459e0c1f0a26f8
daniels <danielsv2008@gmail.com>
2025-11-19 15:49:56 +0200

    as far as i can rember or go

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// your code goes here
+
+const inputfield = document.getElementById("svars")
+const inputfield2 = document.getElementById("augums")
+const button = document.getElementById("dalīt")
+const outputfield = document.getElementById("output")
+
+button.addEventListener('click', calcButton);
+let result = 0
+let result2 = 0
+let result3 = 0
+function calcButton(){
+    result=inputfield2.value/100
+    result2=result**2
+   result3 = inputfield.value/result2
+   output.innerHTML = result3
+   if(result3<=18.99){
+ console.log("Nepietiekama ķermeņa masa")
+   }
+   else if(result3<=24.99){
+     console.log("Normāla ķermeņa masa")
+   }
+    else if(result3<=29.99){
+ console.log("Lieka ķermeņa masa")
+   }
+   else{
+      console.log("Aptaukošanās")
+   }
+}
